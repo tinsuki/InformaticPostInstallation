a7b37aa3b3267ada0e34c686bd39b5433aae869a
tinsuki <83704875+tinsuki@users.noreply.github.com>
2022-12-09 21:40:53 +0100

    Update users.sh

--- users.sh
+++ users.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
-sudo groupaddchefs
-sudo install -d -m 0770 -g chef "/home/partageChefs"
+sudo groupadd chefs
+sudo install -d -m 0770 -g chefs "/home/partageChefs"
 sudo mkdir -m 0777 "/home/partageTous"
 {
     read
     while IFS=\; read -r firstname lastname password group; do
-        lowGroup="${group,,}"
-        shareFolder="partage${lowGroup^}"
-        if [ "$group" = "MARK" ]; then
+        group="${group,,}"
+        shareFolder="partage${group^}"
+        if [ "$group" = "mark" ]; then
             group="marketing"
-        elif [ "$group" = "DEV" ]; then
-            group="dev"
-        else [ "$group" = "WEB"]; then
-            group="web"
         fi
         firstname="${firstname,,}"
         lastname="${lastname,,}"
         username="${firstname:0:1}${lastname:0:6}"
-        basedir="/home/${group,,}"
+        basedir="/home/$group"
         homedir="$basedir/$username"
         if [ $(getent group "$group") ]; then
-          sudo useradd -N -G "$group" -m -b "$basedir" -d "$homedir" -m -k "/skel" "$username"
+          sudo useradd -N -G "$group" -m -k "/skel" "$username"
           echo "$username:$password" | sudo chpasswd
           sudo chmod 700 "$homedir"
           sudo ln -s "$basedir/$shareFolder" "$homedir"
@@ -28,7 +24,7 @@ sudo mkdir -m 0777 "/home/partageTous"
         else
           sudo groupadd "$group"
           sudo mkdir "$basedir"
-          sudo useradd -N -G CHEFS,"$group" -m -b "$basedir" -d "$homedir" -k "/skel" "$username"
+          sudo useradd -N -G chefs,"$group" -m -k "/skel" "$username"
           echo "$username:$password" | sudo chpasswd
           sudo chmod 700 "$homedir"
           sudo mkdir "$basedir/$shareFolder"
