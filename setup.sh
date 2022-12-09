ae1b056bd264a3f4b3dcf8e55b3ab3d7bb3fca46
tinsuki <83704875+tinsuki@users.noreply.github.com>
2022-12-09 15:12:03 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ echo 'install zip for dependencies'
 echo '##########################################'
 sudo pacman -S unzip --noconfirm
 unzip skel -d skel
+sudo chmod +x users.sh
+sudo chmod +x installApp.sh
 echo '##########################################'
 echo 'Creare Users and Groups'
 echo '##########################################'
